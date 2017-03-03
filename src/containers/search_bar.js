7f7612fc907e7bd16958d7a08ad1a2df8a3a37fd
Þorvarðu Örn Einarsson <thorrik58@gmail.com>
2017-03-03 03:46:25 +0000

    loads of changes for react promises and middleware.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
+import { connect} from 'react-redux';
+import { bindActionCreators } from 'redux'
+import { fetchWeather } from '../actions/index'
 
-import config from '../../config.json'
-
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props);
 
     this.state = { term : ''};
 
     this.onInputChange = this.onInputChange.bind(this);
+    // this.props was returning empty due to wrong this, needed to bind it to
+    // the searchbar this
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
-    console.log(event.target.value);
-    console.log(config.API_KEY)
     this.setState({ term: event.target.value });
   }
 
   //In order to prevent the page from refreshing when pressing enter or submit
   onFormSubmit(event) {
     event.preventDefault();
+
+    // Calls our action creator with the search term
+    this.props.fetchWeather(this.state.term);
+    // Sets the search term to empty string on submit
+    this.setState({ term: ''});
   }
 
 
@@ -42,3 +49,12 @@ export default class SearchBar extends Component {
 
 
 }
+// gives us access to the function this.props.fetchWeather in the SearchBar
+// component
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// tells that you need to connect the two but we dont care about the state in
+// this container
+export default connect(null, mapDispatchToProps)(SearchBar);
