58ee5ddd2fdb1a73bd964d27921f6ecd37ec4fdd
Þorvarðu Örn Einarsson <thorrik58@gmail.com>
2017-03-06 17:19:50 +0000

    City search coming along after a small break from the project

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,13 @@
-export default function(state = null, action) {
-  console.log('action received', action);
+import FETCH_WEATHER from '../actions/index';
 
+export default function(state = [], action) {
+  switch (action.type){
+    case FETCH_WEATHER:
+      // Mutates the array, so we use the new es6 method
+      // return state.concat([action.payload.data]);
+      // ES6 way of concatinating the two arrays, returns a new array in the
+      // form of [city, city, city] not [city, [city, city]]
+      return [ action.payload.data, ...state ];
+  }
   return state;
 }
